58fb4527bc24743534d763664d8e1cb95244a0aa
DolaRauf <99263745+Gimeliberty@users.noreply.github.com>
2024-02-27 21:20:40 +0100

    27 feb

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,34 +2,49 @@ import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Button } from '../../../components/Button';
+import { Container } from '../../../components/Container';
+import { theme } from '../../../styles/Theme';
 
 export const Contact = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-                <Field placeholder={"name"}/>
-                <Field placeholder={"subject"}/>
-                <Field placeholder={"message"} as={"textarea"}/>
-                <Button type={"submit"} >Send message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact</SectionTitle>
+                    <StyledForm>
+                        <Field placeholder={"name"}/>
+                        <Field placeholder={"subject"}/>
+                        <Field placeholder={"message"} as={"textarea"}/>
+                        <Button type={"submit"} >Send message</Button>
+                    </StyledForm>
+            </Container>
         </StyledContacts>
     );
 };
 
 const StyledContacts = styled.section `
-    min-height: 50vh;
-    background-color: #e8f57f;
+
 `
 const StyledForm = styled.form `
-    max-width:500px;
+    max-width:540px;
     width:100%;
     display:flex;
     flex-direction:column;
-    gap:10px;
-    margin:0 auto;
+    align-items: center;
+    gap: 16px;
+    margin: 0 auto;
 
 `
 const Field = styled.input `
-    
+    width: 100%;
+    border: 1px solid ${theme.colors.borderColor};
+    background-color: ${theme.colors.secondaryBg};
+    padding: 8px;
+
+    color: #495057;
+    font-family: Poppins;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 0.6px;
 `
